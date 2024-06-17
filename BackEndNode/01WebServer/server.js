a6be4529bc7304061d3cdd903afd70b055b07cc5
James <salehjames01@gmail.com>
2024-06-17 22:32:22 +0100

    new commit

--- BackEndNode/01WebServer/server.js
+++ BackEndNode/01WebServer/server.js
@@ -7,6 +7,6 @@ http.createServer((req, res) => {
     res.writeHead(200, {'Content-type': 'text/plain'});
     res.end('Hello World');
 }).listen(port, hostname, () => {
-    console.log(`Server is running at http://${hostname}:${port}`);
+    console.log(`Server is running at http://${hostname}:${port}/`);
 
 });
