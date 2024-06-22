f3759d870a4102bdbd4ca0bcc378c02a2db9a429
James <salehjames01@gmail.com>
2024-06-22 01:38:49 +0100

    new commit

--- BackEndNode/03expressbasics/index.js
+++ BackEndNode/03expressbasics/index.js
@@ -6,4 +6,18 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 }) 
 
+app.get('/about', (req, res) => {
+    res.send('<h1>I am about page</h1>');
+});
+
+app.post('/login', (req, res) => {
+    res.send("login success");
+});
+
+app.delete('/delete', (req, res) => (
+    res.send("deleted successfully")
+));
+
+// create route for contact us and services
+
 app.listen(3000, () => console.log('Server is running on port 3000...'));
